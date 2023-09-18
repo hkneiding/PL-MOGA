f291bdd91dd97689fd3f8378c52c6d5596773dde
Hannes Kneiding <hanneskn@uio.no>
2023-09-18 12:46:37 -0400

    phase out tests

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -7,128 +7,130 @@ from gapy.selection import *
 
 class TestTools(unittest.TestCase):
 
-    @parameterized.expand([
+    # @parameterized.expand([
 
-        [
-            [
-                Individual(genome=[], fitness=[1]),
-                Individual(genome=[], fitness=[2]),
-                Individual(genome=[], fitness=[3]),
-                Individual(genome=[], fitness=[4])
-            ],
-            1,
-            [
-                Individual(genome=[], fitness=[4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[1]),
+    #             Individual(genome=[], fitness=[2]),
+    #             Individual(genome=[], fitness=[3]),
+    #             Individual(genome=[], fitness=[4])
+    #         ],
+    #         1,
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #         ]
+    #     ],
 
-        [
-            [
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4])
-            ],
-            1,
-            [
-                Individual(genome=[], fitness=[4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4])
+    #         ],
+    #         1,
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #         ]
+    #     ],
 
-        [
-            [
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[0])
-            ],
-            2,
-            [
-                Individual(genome=[], fitness=[0]),
-                Individual(genome=[], fitness=[-4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[0])
+    #         ],
+    #         2,
+    #         [
+    #             Individual(genome=[], fitness=[0]),
+    #             Individual(genome=[], fitness=[-4]),
+    #         ]
+    #     ],
 
-        [
-            [
-                Individual(genome=[], fitness=[1, 2]),
-                Individual(genome=[], fitness=[2, 7]),
-                Individual(genome=[], fitness=[3, 5]),
-                Individual(genome=[], fitness=[4, 2])
-            ],
-            2,
-            [
-                Individual(genome=[], fitness=[2, 7]),
-                Individual(genome=[], fitness=[3, 5]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[1, 2]),
+    #             Individual(genome=[], fitness=[2, 7]),
+    #             Individual(genome=[], fitness=[3, 5]),
+    #             Individual(genome=[], fitness=[4, 2])
+    #         ],
+    #         2,
+    #         [
+    #             Individual(genome=[], fitness=[2, 7]),
+    #             Individual(genome=[], fitness=[3, 5]),
+    #         ]
+    #     ],
 
-    ])
-    def test_select_by_fitness(self, individuals, n_selected, expected):
+    # ])
+    # def test_select_by_fitness(self, individuals, n_selected, expected):
 
-        result = select_by_fitness(individuals, n_selected)
+    #     result = select_by_fitness(individuals, n_selected)
 
-        Utils.assert_are_almost_equal(result, expected)
+    #     Utils.assert_are_almost_equal(result, expected)
 
-    @parameterized.expand([
+    # @parameterized.expand([
 
-        [
-            [
-                Individual(genome=[], fitness=[1]),
-                Individual(genome=[], fitness=[2]),
-                Individual(genome=[], fitness=[3]),
-                Individual(genome=[], fitness=[4])
-            ],
-            1,
-            [
-                Individual(genome=[], fitness=[4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[1]),
+    #             Individual(genome=[], fitness=[2]),
+    #             Individual(genome=[], fitness=[3]),
+    #             Individual(genome=[], fitness=[4])
+    #         ],
+    #         1,
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #         ]
+    #     ],
 
-        [
-            [
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4]),
-                Individual(genome=[], fitness=[4])
-            ],
-            1,
-            [
-                Individual(genome=[], fitness=[4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4]),
+    #             Individual(genome=[], fitness=[4])
+    #         ],
+    #         1,
+    #         [
+    #             Individual(genome=[], fitness=[4]),
+    #         ]
+    #     ],
 
-        [
-            [
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[-4]),
-                Individual(genome=[], fitness=[0])
-            ],
-            2,
-            [
-                Individual(genome=[], fitness=[0]),
-                Individual(genome=[], fitness=[-4]),
-            ]
-        ],
+    #     [
+    #         [
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[-4]),
+    #             Individual(genome=[], fitness=[0])
+    #         ],
+    #         2,
+    #         [
+    #             Individual(genome=[], fitness=[0]),
+    #             Individual(genome=[], fitness=[-4]),
+    #         ]
+    #     ],
 
-                [
-            [
-                Individual(genome=[], fitness=[0, 0]),
-                Individual(genome=[], fitness=[1, 0]),
-                Individual(genome=[], fitness=[1, 1]),
-                Individual(genome=[], fitness=[2, 0])
-            ],
-            2,
-            [
-                Individual(genome=[], fitness=[1, 1]),
-                Individual(genome=[], fitness=[2, 0]),
-            ]
-        ],
+    #             [
+    #         [
+    #             Individual(genome=[], fitness=[0, 0]),
+    #             Individual(genome=[], fitness=[1, 0]),
+    #             Individual(genome=[], fitness=[1, 1]),
+    #             Individual(genome=[], fitness=[2, 0])
+    #         ],
+    #         2,
+    #         [
+    #             Individual(genome=[], fitness=[1, 1]),
+    #             Individual(genome=[], fitness=[2, 0]),
+    #         ]
+    #     ],
 
-    ])
-    def test_pareto_domination_rank(self, individuals, n_selected, expected):
+    # ])
+    # def test_pareto_domination_rank(self, individuals, n_selected, expected):
 
-        result = pareto_domination_rank(individuals, n_selected)
+    #     result = pareto_domination_rank(individuals, n_selected)
 
-        Utils.assert_are_almost_equal(result, expected)
+    #     Utils.assert_are_almost_equal(result, expected)
+
+    pass
